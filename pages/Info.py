417d1283a1bf989fc3205b04c69bae6d636e8382
Kaleb-Nim <65989471+Kaleb-Nim@users.noreply.github.com>
2023-05-28 17:01:57 +0800

    Integrate Mo's thigny with AR

---
+++ pages/Info.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.markdown("""
+# EduPlay by Cotton Pickers
+
+
+## Main Features:
+
+
+### ✨📚 *AR Interactive Lessons:*
+- Engaging lessons with visually stimulating content and gamified activities
+- AR technology brings textbooks to life with 360-degree, 3D animations
+
+### 🎨✏️ *AI Drawing Tools:*
+- Express creativity with hand gestures for drawing, slide manipulation, and gaming
+- Cutting-edge computer vision and AI technology revolutionize interaction
+             """)
